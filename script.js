99fb2779f30e5d3cbed2e48402f63cb0f594f6bc
znfnfns0365 <znfnfns0365@naver.com>
2024-04-28 16:57:41 +0900

    finish

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let searchButton = document.getElementById("searchButton");
+let searchInput = document.getElementById("searchInput");
+
 const options = {
     method: 'GET',
     headers: {
@@ -13,10 +16,11 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
 
 
 function displayMovies(data) {
+    this.data = data;
     data.results.forEach(movie => { // alert에서 띄어쓰기 하면 오류나는 이유..?
+
         let movieCode = `<div onclick="alert('영화 id: ' + '${movie.id}')" id="${movie.id}" class="card">
-        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
-            class="card-img-top">
+        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="card-img-top">
             <div class="card-body">
                 <h2 class="card-title">${movie.title}</h2>
                 <p class="card-text">${movie.overview}</p>
@@ -25,4 +29,34 @@ function displayMovies(data) {
         const editId = document.getElementById("cardSpace");
         editId.innerHTML += movieCode;
     });
+    let search = () => {
+        const editId = document.getElementById("cardSpace");
+        editId.innerHTML = null;
+        const elements = document.getElementsByClassName("cards");
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.margin = "0px";
+        }
+        let Arr = Array.from(data.results);
+        Arr = Arr.filter(function (obj) {
+            const cmp=obj.title.toLowerCase();
+            return cmp.includes(searchInput.value.toLowerCase());
+        });
+        for (let i = 0; i < Arr.length; i++) {
+            const movie=Arr[i];
+            let movieCode = `<div onclick="alert('영화 id: ' + '${movie.id}')" id="${movie.id}" class="card">
+                <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="card-img-top">
+                <div class="card-body">
+                    <h2 class="card-title">${movie.title}</h2>
+                    <p class="card-text">${movie.overview}</p>
+                </div>
+            </div>`
+            const editId = document.getElementById("cardSpace");
+            editId.innerHTML += movieCode;
+        }
+        return;
+    }
+    searchButton.addEventListener('click', search);
 }
+
+
+
